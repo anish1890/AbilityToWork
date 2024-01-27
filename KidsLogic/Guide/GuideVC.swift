edb312b1a7513bde7e03aaebd90140a87870f10a
Anish <anish1890@gmail.com>
2024-01-27 08:13:16 +0500

    first push

---
+++ KidsLogic/Guide/GuideVC.swift
@@ -0,0 +1,24 @@
+//
+//  GuideVC.swift
+//  AbilityToHelp
+//
+//  Created by Anish on 1/27/24.
+//
+
+import UIKit
+
+class GuideVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clsoe(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+
+
+}
